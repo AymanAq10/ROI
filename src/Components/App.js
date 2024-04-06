19e43ac1d7f3d5988486992ebf40d37d3581aa23
Ayman <aakartit@gmail.com>
2024-04-06 00:58:42 +0000

    ee

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,7 @@ import Home from "./Component/Home";
 // import Footer from "./Component/Footer";
 import Header from "./Component/Header"
 import './App.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
                 <Header />
                 <main className="container">
                     <Home />
+                    <SpeedInsights />
                 </main>
             </div>
     )
